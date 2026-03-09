aee7158c180af84d3791403dadd9f50c711b120b
D4RY-exe <129776733+D4RY-exe@users.noreply.github.com>
2026-03-09 14:51:20 -0400

    update urls.py to have register and login

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import register_user, login_user
 
 urlpatterns = [
+    path('register/', register_user, name='register_user'),
+    path('login/', login_user, name='login_user'),
     path('<int:user_id>/preferred-words/', views.preferred_words, name='preferred_words'),
 ]
