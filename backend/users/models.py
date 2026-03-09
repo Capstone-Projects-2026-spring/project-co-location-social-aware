d8e82df57e02ae313fbb77ad28925ec185021e46
D4RY-exe <129776733+D4RY-exe@users.noreply.github.com>
2026-03-09 14:45:54 -0400

    Getting a start on backend admin profile saving; added serializer file

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 class User(models.Model):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('child', 'Child'),
+    )
+
     username = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    role = models.CharField(max_length=255, choices=ROLE_CHOICES, default="admin")
     room = models.ForeignKey('beacons.Beacon', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.email
