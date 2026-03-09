f854ce6c651adb8c71b676bcc0174f8db3af2538
D4RY-exe <129776733+D4RY-exe@users.noreply.github.com>
2026-03-08 22:31:39 -0400

    updating register.tx to include child and admin creation

--- claac/app/register.tsx
+++ claac/app/register.tsx
@@ -4,10 +4,29 @@ import { Text, View, Image, StyleSheet, Pressable } from "react-native";
 import { styles } from "../styles/homePageStyle";
 import { Button } from "@react-navigation/elements";
 import { router, useRouter } from "expo-router";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+const Tab = createMaterialTopTabNavigator();
+
+function AdultAccount() {
+  return (
+    <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+      <Text>Adult Account Creation</Text>
+    </View>
+  );
+}
+
+function ChildAccount() {
+  return (
+    <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+      <Text>Child Account Creation</Text>
+    </View>
+  );
+}
 
 export default function RegisterScreen() { // Must be 'export default'
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex:1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Register Screen</Text>
       <View style={styles.buttonContainer}>
                     <Pressable style={styles.button}
